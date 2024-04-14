869ac475efdd90384a5d4a58af9686d35fe9a7e4
Võ Minh Sang <145260255+VMSang@users.noreply.github.com>
2024-04-14 19:28:10 +0700

    Create waterMellon.cpp
    first commit

---
+++ CodeForces/waterMellon.cpp
@@ -0,0 +1,28 @@
+/*
+	username: Minh Sang
+*/
+#include <bits/stdc++.h>
+//======================================
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define sz size
+#define mp make_pair
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vdb vector<double>
+using namespace std;
+//======================================
+
+int main(){ 
+	int n; cin>>n;
+	if (n%2 == 0 && n>2)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;	
+	return 0;
+}
